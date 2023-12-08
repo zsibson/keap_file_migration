c4ddb1c788a9bb5cca008d7b868f2f7ddf5a06c4
Zachary Sibson <zacmfp@zacharys-air.lan>
2023-12-08 16:24:28 -0700

    authentication working

--- download_files.py
+++ download_files.py
@@ -1,108 +1,78 @@
-import requests   
-from dotenv import load_dotenv
+import requests
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-#Authentication
-
+# Your Keap API credentials
 client_id = os.getenv('KEAP_CLIENT_ID')
 client_secret = os.getenv('KEAP_CLIENT_SECRET')
-scope = 'full'
+redirect_uri = os.getenv('KEAP_REDIRECT_URI')
+access_token = os.getenv('KEAP_ACCESS_TOKEN')
+refresh_token = os.getenv('KEAP_REFRESH_TOKEN')
 
-# Redirect to Keap for Authentication 
+# Base URL for Keap API
+api_base_url = "https://api.infusionsoft.com/crm/rest/v1"
 
-ngrok_url = 'https://475f-69-146-65-82.ngrok-free.app'
-redirect_uri = f'{ngrok_url}/callback'
-
-authorization_url = 'https://accounts.infusionsoft.com/app/oauth/authorize'
-authorization_params = {
-    'client_id' : client_id,
-    'redirect_uri' : redirect_uri,
-    'response_type' : 'code', 
-    'scope' : scope
-}
-
-authorization_response = requests.get(authorization_url, params=authorization_params)
-
-# Assuming you have a web application, redirect the user to the authorization URL
-# or print the URL and have the user manually visit it
-
-# Example print statement for manual user visit
-print("Visit the following URL to authorize your application:")
-print(authorization_response.url)
-
-# Capture the authorization code from the redirected URL or user input
-authorization_code = input("Enter the authorization code: ")
-
-# Step 2: Request an access token using the authorization code
-token_url = 'https://api.infusionsoft.com/token'
-token_payload = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'code': authorization_code,
-    'grant_type': 'authorization_code',
-    'redirect_uri': redirect_uri
-}
-
-token_response = requests.post(token_url, data=token_payload)
-
-# Check if the response is successful
-if token_response.status_code == 200:
-    # Retrieve the access token from the response
-    access_token = token_response.json().get('access_token')
-
-    # Now you can use the obtained access token to make API requests
-    api_url = 'https://api.infusionsoft.com/crm/rest/v1/files'  # Replace with your desired API endpoint
-    api_headers = {
-        'Authorization': f'Bearer {keap_access_token}'
+# Function to refresh access token using the refresh token
+def refresh_access_token():
+    refresh_url = "https://api.infusionsoft.com/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {base64_encode(client_id + ':' + client_secret)}"
     }
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+    response = requests.post(refresh_url, headers=headers, data=data)
+    response_data = response.json()
+    return response_data.get("access_token")
 
-    # Example: Make an API request using the obtained access token
-    api_response = requests.get(api_url, headers=api_headers)
+# Function to get all file IDs
+def get_all_file_ids(access_token):
+    files_url = f"{api_base_url}/files"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(files_url, headers=headers)
+    return response.json()
 
-    print(api_response.json())  # Print the API response
-else:
-    print(f"Error: {token_response.status_code} - {token_response.text}")
+# Function to download files to a local directory
+def download_files(file_ids, directory_path):
+    for file_id in file_ids:
+        file_url = f"https://api.infusionsoft.com/crm/rest/v1/files/{file_id}/download"
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        response = requests.get(file_url, headers=headers)
+        
+        # Use a meaningful file name (you may extract it from the response headers)
+        file_name = f"file_{file_id}.txt"
+        
+        file_path = os.path.join(directory_path, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
 
+# Helper function to encode client_id and client_secret for authorization header
+def base64_encode(s):
+    import base64
+    return base64.b64encode(s.encode()).decode()
 
-# #Get File Information 
+# Check if the access token is still valid, refresh if necessary
+if not access_token:
+    access_token = refresh_access_token()
 
-# files_endpoint = 'https://api.infusionsoft.com/crm/rest/v1/files'
-# download_base_url = 'https://api.infusionsoft.com/crm/rest/v1/files/{}/download'
-# keap_access_token = os.getenv("ACCESS_TOKEN")
-# local_directory = '/Users/zacmfp/keap_migration_files'
+# Get all file IDs from the Keap API
+all_file_ids = get_all_file_ids(access_token)
 
+# Specify the directory path for downloading files
+directory_path = "downloaded_files"
 
-# # Step 1: Retrieve File Information
-# headers = {
-#     'Authorization': f'Bearer {keap_access_token}'
-# }
-# response = requests.get(files_endpoint, headers=headers)
+# Create the directory if it doesn't exist
+os.makedirs(directory_path, exist_ok=True)
 
-# # Check if the response is successful
-# if response.status_code == 200:
-#     # Check if the 'files' key is present in the JSON response
-#     if 'files' in response.json():
-#         files_data = response.json()['files']
+# Download files to the local directory
+download_files(all_file_ids, directory_path)
 
-#         # Step 2: Download Files
-#         for file_info in files_data:
-#             file_id = file_info['id']
-#             download_url = download_base_url.format(file_id)
-
-#             download_response = requests.get(download_url, headers=headers)
-
-#             # Extract filename from the Content-Disposition header
-#             content_disposition = download_response.headers.get('Content-Disposition')
-#             filename = content_disposition.split('=')[1] if content_disposition else f'file_{file_id}.pdf'
-
-#             # Save the file to the specified local directory
-#             with open(filename, 'wb') as file:
-#                 file.write(download_response.content)
-
-#             print(f"File {filename} downloaded successfully.")
-#     else:
-#         print("No 'files' key found in the JSON response.")
-# else:
-#     print(f"Error: {response.status_code} - {response.text}")
+print(f"All files downloaded successfully to {directory_path}")
